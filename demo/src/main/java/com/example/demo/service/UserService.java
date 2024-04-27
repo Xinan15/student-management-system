a2cd4b6c2aa56635699b1eb0c237012543d79c5a
Xinan <youkingan11@gmail.com>
2024-04-27 06:13:54 +0100

    boilerplate

---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,32 @@
+// Service layer for User entity
+
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUser(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
